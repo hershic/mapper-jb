63e6d0168f7896157bf5b1a76be324fc0a93a83b
Hershal Bhave <hershal.bhave@gmail.com>
2014-10-13 13:17:12 -0500

    close to getting the map centering working

--- src/com/hershic/mapper_jb/MainActivity.java
+++ src/com/hershic/mapper_jb/MainActivity.java
@@ -2,27 +2,21 @@ package com.hershic.mapper_jb;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Iterator;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import org.apache.http.HttpResponse;
 import org.apache.http.HttpStatus;
 import org.apache.http.StatusLine;
-import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.DefaultHttpClient;
-import org.json.JSONException;
-import org.json.JSONObject;
 
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.FragmentManager;
 import android.content.Context;
 import android.content.DialogInterface;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -37,59 +31,54 @@ import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 
-class RequestTask extends AsyncTask<String, String, String> {
+public class MainActivity extends Activity {
 
-	private Context context;
-
-	public void setContext(Context ctx) {
-		context = ctx;
-	}
-
-	protected String doInBackground(String... uri) {
-		HttpClient httpclient = new DefaultHttpClient();
-		HttpResponse response;
-		String responseString = null;
+	public void doPost(String url) {
 		try {
-			response = httpclient.execute(new HttpGet(uri[0]));
+			HttpClient httpclient = new DefaultHttpClient();
+			HttpResponse response = httpclient.execute(new HttpGet(url));
 			StatusLine statusLine = response.getStatusLine();
 			if (statusLine.getStatusCode() == HttpStatus.SC_OK) {
 				ByteArrayOutputStream out = new ByteArrayOutputStream();
 				response.getEntity().writeTo(out);
+				String responseString = out.toString();
+				// ..more logic
 				out.close();
-				responseString = out.toString();
+				setLatLon(responseString);
 			} else {
 				// Closes the connection.
 				response.getEntity().getContent().close();
 				throw new IOException(statusLine.getReasonPhrase());
 			}
-		} catch (ClientProtocolException e) {
-			// TODO Handle problems..
-		} catch (IOException e) {
-			// TODO Handle problems..
+		} catch (Exception e) {
+			Log.v("FUCKERS", "Done killed all the stuff");
 		}
-		return responseString;
 	}
 
-	public HashMap<String, String> jsonToMap(String t) throws JSONException {
+	final Context context = this;
+	static String lat;
+	static String lon;
+	private FragmentManager fragmgr;
 
-		HashMap<String, String> map = new HashMap<String, String>();
-		JSONObject jObject = new JSONObject(t);
-		Iterator<?> keys = jObject.keys();
-
-		while (keys.hasNext()) {
-			String key = (String) keys.next();
-			String value = jObject.getString(key);
-			map.put(key, value);
-			Log.v("FOUND_KEY", "key: " + key + " value: " + value + "\n");
-		}
-
-		return (map);
+	public void moveMapToLocation(String lat, String lon) {
+		LatLng latlng = new LatLng(Double.parseDouble(lat),
+				Double.parseDouble(lon));
+		// Construct a CameraPosition focusing on Mountain View and animate the
+		// camera to that position.
+		CameraPosition cameraPosition = new CameraPosition.Builder()
+				.target(latlng) // Sets the center of the map to Mountain View
+				.zoom(2) // Sets the zoom in dp (density-independent pixels)
+				.bearing(0) // Sets the orientation of the camera to North
+				.tilt(30) // Sets the tilt of the camera to 30 degrees
+				.build(); // Creates a CameraPosition from the builder
+		Log.v("CAMERA", "Lat: " + lat);
+		Log.v("CAMERA", "Long: " + lon);
+		((MapFragment) fragmgr.findFragmentById(R.id.map)).getMap()
+				.animateCamera(
+						CameraUpdateFactory.newCameraPosition(cameraPosition));
 	}
 
-	@Override
-	protected void onPostExecute(String result) {
-		super.onPostExecute(result);
-		// Do anything with response..
+	public void setLatLon(String result) {
 		Log.v("GOT_DATA", result);
 		String printresults = "Nothing Found";
 
@@ -105,53 +94,22 @@ class RequestTask extends AsyncTask<String, String, String> {
 		// if our pattern matches the string, we can try to extract our groups
 		if (m.find()) {
 			// get the two groups we were looking for
-			group1 = m.group(1);
-			group2 = m.group(2);
+			lat = m.group(1);
+			lon = m.group(2);
 
 			// print the groups, with a wee bit of formatting
 			System.out.format("'%s', '%s'\n", group1, group2);
 			printresults = group1 + " " + group2;
 		}
 		Toast.makeText(context, printresults, Toast.LENGTH_LONG).show();
-		MainActivity.setLatLong(group1, group2);
+		moveMapToLocation(lat, lon);
 	}
-}
 
-public class MainActivity extends Activity {
-
-	final Context context = this;
-	static String lat;
-	static String lon;
-	static FragmentManager fragmgr;
-	
-	public static void moveMapToLocation(String lat, String lon) {
-		LatLng latlng = new LatLng(Double.parseDouble(lat),
-				Double.parseDouble(lon));
-		// Construct a CameraPosition focusing on Mountain View and animate the
-		// camera to that position.
-		CameraPosition cameraPosition = new CameraPosition.Builder()
-				.target(latlng) // Sets the center of the map to Mountain View
-				.zoom(2) // Sets the zoom in dp (density-independent pixels)
-				.bearing(0) // Sets the orientation of the camera to North
-				.tilt(30) // Sets the tilt of the camera to 30 degrees
-				.build(); // Creates a CameraPosition from the builder
-		((MapFragment)fragmgr.findFragmentById(R.id.map)).getMap().animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
-	}
-	
-	public static void setLatLong(String la, String lo) {
-		lat = la;
-		lon = lo;
-		moveMapToLocation(lat,lon);
-	}
-	
-	public static void setFragmentManager(FragmentManager fm) {
-		fragmgr = fm;
-	}
-	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		fragmgr = getFragmentManager();
 	}
 
 	@Override
@@ -193,9 +151,7 @@ public class MainActivity extends Activity {
 
 									String input_escaped = userInput.getText()
 											.toString().replaceAll(" ", "+");
-									RequestTask task = new RequestTask();
-									task.setContext(context);
-									task.execute("https://maps.googleapis.com/maps/api/geocode/json?address="
+									doPost("https://maps.googleapis.com/maps/api/geocode/json?address="
 											+ input_escaped
 											+ "&key=AIzaSyABIsx_rPFKIqctKcGXZ7-0lWWKQQNNS8w");
 								}
