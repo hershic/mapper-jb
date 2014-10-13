b7bdf63f8921c0cdcc56bb25c22b57840b1c7fab
Hershal Bhave <hershal.bhave@gmail.com>
2014-10-13 10:35:17 -0500

    map works

---
+++ src/com/hershic/mapper_jb/MapFragment.java
@@ -0,0 +1,26 @@
+package com.hershic.mapper_jb;
+
+import android.app.Activity;
+import android.app.ActionBar;
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.os.Build;
+
+public class MapFragment extends Fragment {
+
+	public MapFragment() {
+	}
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		View rootView = inflater.inflate(R.layout.activity_main, container,
+				false);
+		return rootView;
+	}
+}
